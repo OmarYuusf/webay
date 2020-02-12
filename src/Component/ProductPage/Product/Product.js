34ffdaa955e1ab910ac623030cb5e6ee24245b95
OmarYuusf <57051225+OmarYuusf@users.noreply.github.com>
2020-02-11 21:12:33 -0800

    fix open imgs

--- src/Component/ProductPage/Product/Product.js
+++ src/Component/ProductPage/Product/Product.js
@@ -1,168 +1,218 @@
-import React from 'react';
-import "./pro.css"
-// FontAwsome 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
-import { faUndo } from '@fortawesome/free-solid-svg-icons'
-import { faStoreAlt } from '@fortawesome/free-solid-svg-icons'
-import { faTruckMoving } from '@fortawesome/free-solid-svg-icons'
-import { faUserShield } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import "./pro.css";
+// FontAwsome
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faUndo } from "@fortawesome/free-solid-svg-icons";
+import { faStoreAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTruckMoving } from "@fortawesome/free-solid-svg-icons";
+import { faUserShield } from "@fortawesome/free-solid-svg-icons";
 
-
-import ReactImageMagnify from 'react-image-magnify';
 
 // Styled-Component
-import styled from 'styled-components'
+import styled from "styled-components";
 
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from "react-bootstrap";
 
-const Product = styled.div`
-    
-`
+const Product = styled.div``;
+const images = [
+  "https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg",
+  "https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg",
+  "https://k.nooncdn.com/t_desktop-pdp-v1/v1556008059/N23423233A_2.jpg",
+  "https://k.nooncdn.com/t_desktop-pdp-v1/v1570191590/N23423233A_4.jpg",
+  " https://k.nooncdn.com/t_desktop-pdp-v1/v1570191590/N23423233A_7.jpg",
+  "https://k.nooncdn.com/t_desktop-pdp-v1/v1570191590/N23423233A_6.jpg"
+];
+
 class ProductItems extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            imgSrc : "https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg",
-            img : [
-                {src: "https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg", alt:"product"},
-                {src: "https://k.nooncdn.com/t_desktop-pdp-v1/v1556008059/N23423233A_2.jpg", alt:"product"},
-                {src: "https://k.nooncdn.com/t_desktop-pdp-v1/v1570191589/N23423233A_3.jpg", alt:"product"},
-                {src: "https://k.nooncdn.com/t_desktop-pdp-v1/v1570191590/N23423233A_4.jpg", alt:"product"},
-                {src: "https://k.nooncdn.com/t_desktop-pdp-v1/v1570191590/N23423233A_6.jpg", alt:"product"},
-                {src: "https://k.nooncdn.com/t_desktop-pdp-v1/v1570191590/N23423233A_7.jpg", alt:"product"}
-            ],
-            quantity:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-        }
-    }
+    this.state = {
+      primImg: images[0],
+      photoIndex: 0,
+      isOpen: false,
+      quantity: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    };
+  }
 
-    handleChangeImg = (e) => {
-        this.setState({
-            imgSrc: e.target.src
-        })
-    }
+  handleChangeImg = e => {
+    var index = images.findIndex(items => {
+      return items == e.target.src;
+    });
+    this.setState({
+      primImg: e.target.src,
+      photoIndex: index
+    });
+  };
 
+  render() {
+    const { photoIndex, isOpen, img } = this.state;
 
+    return (
+      <div className="product">
+        <Container fluid={true}>
+          <Row>
+            <Col xs={12} className="header">
+              <a href="#">الرئيسية</a>
+              {this.props.language === "en" ? (
+                <FontAwesomeIcon
+                  icon={faChevronRight}
+                  size="xs"
+                ></FontAwesomeIcon>
+              ) : (
+                <FontAwesomeIcon
+                  icon={faChevronLeft}
+                  size="xs"
+                ></FontAwesomeIcon>
+              )}
+              <a href="#">اسم القسم</a>
+              {this.props.language === "en" ? (
+                <FontAwesomeIcon
+                  icon={faChevronRight}
+                  size="xs"
+                ></FontAwesomeIcon>
+              ) : (
+                <FontAwesomeIcon
+                  icon={faChevronLeft}
+                  size="xs"
+                ></FontAwesomeIcon>
+              )}
+              <a href="#">اسم المنتج</a>
+            </Col>
 
-    render() {
-        return(
-            <div className="product">
-                <Container fluid={true}>
-                    <Row>
-                        <Col xs={12} className="header">
-                            <a href='#'>الرئيسية</a> 
-                            {this.props.language === 'en' ?(<FontAwesomeIcon icon={faChevronRight} size='xs'></FontAwesomeIcon>) :
-                                                            (<FontAwesomeIcon icon={faChevronLeft} size='xs'></FontAwesomeIcon>)}
-                            <a href='#'>اسم القسم</a> 
-                            {this.props.language === 'en' ?(<FontAwesomeIcon icon={faChevronRight} size='xs'></FontAwesomeIcon>) :
-                                                            (<FontAwesomeIcon icon={faChevronLeft} size='xs'></FontAwesomeIcon>)}
-                            <a href='#'>اسم المنتج</a> 
-                        </Col>
+            <Col xs={12} className="product">
+              <div className="information">
+                <div>
+                  <p className="owner">اسم الشركه</p>
+                  <h5 className="products-name">
+                    هاتف Y6 إصدار (2019) ثنائي الشريحة لون أزرق ياقوتي بذاكرة
+                    رام سعة 2 جيجابايت وذاكرة داخلية سعة 32 جيجابايت ويدعم تقنية
+                    4G LTE
+                  </h5>
+                  <p className="model-number">اسم الموديل</p>
+                  <div className="old-price">
+                    <p> السعر القديم:</p>
+                    <p> 223 جنيه</p>
+                  </div>
+                  <div className="new-price">
+                    <span>
+                      <p> السعر الجديد: </p>
+                      <p> 223 جنيه</p>
+                    </span>
+                    <p className="discount">خصم 15%</p>
+                  </div>
+                </div>
+                <form>
+                  <h6>Quantity</h6>
+                  <div>
+                    <select>
+                      {this.state.quantity.map(item => {
+                        return (
+                          <option key={Math.random()} value={item}>
+                            {item}
+                          </option>
+                        );
+                      })}
+                    </select>
+                    <input type="submit" value="ADD TO CART" />
+                  </div>
+                </form>
+              </div>
+              <div className="imgs">
+                <div className="containerImgs">
+                  {images.map(imgs => {
+                    return (
+                      <img
+                        key={Math.random()}
+                        src={imgs}
+                        alt="product"
+                        onClick={e => this.handleChangeImg(e)}
+                      />
+                    );
+                  })}
+                </div>
+                <div>
+                  <img
+                    src={this.state.primImg}
+                    alt="ahh"
+                    onClick={() => this.setState({ isOpen: true })}
+                  />
+                </div>
+              </div>
 
-                        <Col xs={12} className="product">
-                            <div className="information">
-                                <div>
-                                    <p className="owner">اسم الشركه</p>
-                                    <h5 className="products-name">هاتف Y6 إصدار (2019) ثنائي الشريحة لون أزرق ياقوتي بذاكرة رام سعة 2 جيجابايت وذاكرة داخلية سعة 32 جيجابايت ويدعم تقنية 4G LTE</h5>
-                                    <p className="model-number">اسم الموديل</p>
-                                    <div className="old-price">
-                                        <p>  السعر القديم:</p>
-                                        <p>  223 جنيه</p>
-                                    </div>
-                                    <div className="new-price">
-                                        <span>
-                                            <p>  السعر الجديد: </p>
-                                            <p>   223 جنيه</p>
-                                        </span>
-                                        <p className="discount">خصم 15%</p>
-                                    </div>
-                                    
-                                </div>
-                                <form>
-                                    <h6>Quantity</h6>
-                                    <div>
-                                        <select>
-                                            {
-                                                this.state.quantity.map(item => {
-                                                    return(
-                                                        <option key={Math.random()} value={item}>{item}</option>
-                                                    )
-                                                })
-                                            }
-                                        </select>
-                                        <input type="submit" value="ADD TO CART"/>
-                                    </div>
-                                </form>
-                            </div>
-                            <div className="imgs">
-                                    <div className="containerImgs">
-                                        {this.state.img.map(imgs => {
-                                            return(
-                                                <img key={Math.random()}
-                                                     src={imgs.src}
-                                                     alt="product" 
-                                                     onClick={(e) => this.handleChangeImg(e)} 
-                                                     />
-                                            )
-                                        })}
-                                    </div>
-                                    <div>
-                                        {/* <Magnifier src={this.state.imgSrc} width={500} /> */}
-                                        <ReactImageMagnify {...{
-                                            smallImage: {
-                                                alt: 'product',
-                                                isFluidWidth: true,
-                                                src: this.state.imgSrc
-                                            },
-                                            largeImage: {
-                                                src: this.state.imgSrc,
-                                                srcSet: this.state.imgSrc,
-                                                style:{left:'0px !important'},
-                                                className:"hello"
-                                            }
-                                        }} />
-                                    </div>
-                                </div>
+              <div>
+                {isOpen && (
+                  <Lightbox
+                    mainSrc={images[photoIndex]}
+                    nextSrc={images[(photoIndex + 1) % images.length]}
+                    prevSrc={
+                      images[(photoIndex + images.length - 1) % images.length]
+                    }
+                    onCloseRequest={() => this.setState({ isOpen: false })}
+                    onMovePrevRequest={() =>
+                      this.setState({
+                        photoIndex:
+                          (photoIndex + images.length - 1) % images.length
+                      })
+                    }
+                    onMoveNextRequest={() =>
+                      this.setState({
+                        photoIndex: (photoIndex + 1) % images.length
+                      })
+                    }
+                  />
+                )}
+              </div>
 
-
-                                <div className="ship">
-                                    <div>
-                                        <div>
-                                            <FontAwesomeIcon icon={faStar} size='xs'></FontAwesomeIcon>
-                                            <p>ضمان لمدة عامين</p>
-                                        </div>
-                                        <div>
-                                            <FontAwesomeIcon icon={faUndo} size='xs'></FontAwesomeIcon>
-                                            <p>تقدر دلوقتي ترجّع المنتجات بسهولة في العرض ده.</p>
-                                        </div>
-                                        <div>
-                                            <FontAwesomeIcon icon={faStoreAlt} size='xs'></FontAwesomeIcon>
-                                            <p>  البائع ECS Distribution</p>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div>
-                                            <FontAwesomeIcon icon={faTruckMoving} size='xs'></FontAwesomeIcon>
-                                            <p>الشحن المجاني لما تشتري بـ 250ج.م أو أكتر</p>
-                                        </div>
-                                        <div>
-                                            <FontAwesomeIcon icon={faUndo} size='xs'></FontAwesomeIcon>
-                                            <p>إرجاع مجاني وبسهولة للمنتجات اللي ينفع ترجع</p>
-                                        </div>
-                                        <div>
-                                            <FontAwesomeIcon icon={faUserShield} size='xs'></FontAwesomeIcon>
-                                            <p>متقلقش على بياناتك دايماً في أمان</p>
-                                        </div>
-                                    </div>
-                                </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        )
-    }
+              <div className="ship">
+                <div>
+                  <div>
+                    <FontAwesomeIcon icon={faStar} size="xs"></FontAwesomeIcon>
+                    <p>ضمان لمدة عامين</p>
+                  </div>
+                  <div>
+                    <FontAwesomeIcon icon={faUndo} size="xs"></FontAwesomeIcon>
+                    <p>تقدر دلوقتي ترجّع المنتجات بسهولة في العرض ده.</p>
+                  </div>
+                  <div>
+                    <FontAwesomeIcon
+                      icon={faStoreAlt}
+                      size="xs"
+                    ></FontAwesomeIcon>
+                    <p> البائع ECS Distribution</p>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <FontAwesomeIcon
+                      icon={faTruckMoving}
+                      size="xs"
+                    ></FontAwesomeIcon>
+                    <p>الشحن المجاني لما تشتري بـ 250ج.م أو أكتر</p>
+                  </div>
+                  <div>
+                    <FontAwesomeIcon icon={faUndo} size="xs"></FontAwesomeIcon>
+                    <p>إرجاع مجاني وبسهولة للمنتجات اللي ينفع ترجع</p>
+                  </div>
+                  <div>
+                    <FontAwesomeIcon
+                      icon={faUserShield}
+                      size="xs"
+                    ></FontAwesomeIcon>
+                    <p>متقلقش على بياناتك دايماً في أمان</p>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 export default ProductItems;
--- src/Component/ProductPage/ProductsLike/ProductsLike.js
+++ src/Component/ProductPage/ProductsLike/ProductsLike.js
@@ -5,146 +5,178 @@ import "./ProductsLike.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 
-const ProductsLike = () => {
-  var settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    initialSlide: 0,
-
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+class ProductsLike extends React.Component {
+  state = {
+    src:"",
+    photoIndex: 0,
+    isOpen: false,
   };
-  var settingsSec = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    initialSlide: 0,
 
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
+  render() {
+    var settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      initialSlide: 0,
+
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            initialSlide: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
         }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1
+      ]
+    };
+    var settingsSec = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      initialSlide: 0,
+
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
         }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
-  return (
-    <div className="products-like">
-      <h3>اختار قطعة</h3>
-      <div className="products-like-container">
-        <Slider {...settings}>
-          <div>
-            <div className="products-like-item">
-              <div className="item-head">
-                <div>
-                  <span>
-                    <MDBIcon icon="check-circle" />
-                    <p>اللون</p>
-                  </span>
-                  <span>احمر</span>
-                </div>
-                <div>
-                  <span>
-                    <MDBIcon icon="check-circle" />
-                    <p>وحدة التخزين</p>
-                  </span>
-                  <span>32 جيجا</span>
-                </div>
-                <div>
-                  <span>
-                    <MDBIcon icon="check-circle" />
-                    <p>عرض الشاشة</p>
-                  </span>
-                  <span>12 بوصة</span>
-                </div>
-                <div>
-                  <span>
-                    <MDBIcon icon="check-circle" />
-                    <p>واي فاي</p>
-                  </span>
-                  <span>متواجد</span>
-                </div>
-              </div>
-              <div className="item-center">
-                <Slider {...settingsSec}>
+      ]
+    };
+    const { photoIndex, isOpen } = this.state;
+
+    return (
+      <div className="products-like">
+        <h3>اختار قطعة</h3>
+        <div className="products-like-container">
+          <Slider {...settings}>
+            <div>
+              <div className="products-like-item">
+                <div className="item-head">
                   <div>
-                    <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
+                    <span>
+                      <MDBIcon icon="check-circle" />
+                      <p>اللون</p>
+                    </span>
+                    <span>احمر</span>
                   </div>
                   <div>
-                    <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
+                    <span>
+                      <MDBIcon icon="check-circle" />
+                      <p>وحدة التخزين</p>
+                    </span>
+                    <span>32 جيجا</span>
                   </div>
                   <div>
-                    <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
+                    <span>
+                      <MDBIcon icon="check-circle" />
+                      <p>عرض الشاشة</p>
+                    </span>
+                    <span>12 بوصة</span>
                   </div>
-                </Slider>
-              </div>
-              <div className="item-footer">
-                <h5>99 ريال</h5>
-                <div className="item-footer-discount">
-                  <p>135 ريال </p>
-                  <span>خصم 27 %</span>
+                  <div>
+                    <span>
+                      <MDBIcon icon="check-circle" />
+                      <p>واي فاي</p>
+                    </span>
+                    <span>متواجد</span>
+                  </div>
                 </div>
-                <p className="discount-end">الخصم سينتهي خلال شهرين</p>
-              </div>
-              <div className="item-button-select">
-                <button>اختار القطعة</button>
+                <div className="item-center">
+                  <Slider {...settingsSec}>
+                    <div>
+                      <img
+                        src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg"
+                        onClick={e =>
+                          this.setState({ src: e.target.src, isOpen: true })
+                        }
+                      />
+                    </div>
+                    <div>
+                      <img
+                        src="https://k.nooncdn.com/t_desktop-pdp-v1/v1556008059/N23423233A_2.jpg"
+                        onClick={e =>
+                          this.setState({ src: e.target.src, isOpen: true })
+                        }
+                      />
+                    </div>
+                    <div>
+                      <img
+                        src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191590/N23423233A_7.jpg"
+                        onClick={e =>
+                          this.setState({ src: e.target.src, isOpen: true })
+                        }
+                      />
+                    </div>
+                  </Slider>
+                  {isOpen && (
+                    <Lightbox
+                      mainSrc={this.state.src}
+                      onCloseRequest={() => this.setState({ isOpen: false })}
+                    />
+                  )}
+                </div>
+                <div className="item-footer">
+                  <h5>99 ريال</h5>
+                  <div className="item-footer-discount">
+                    <p>135 ريال </p>
+                    <span>خصم 27 %</span>
+                  </div>
+                  <p className="discount-end">الخصم سينتهي خلال شهرين</p>
+                </div>
+                <div className="item-button-select">
+                  <button>اختار القطعة</button>
+                </div>
               </div>
             </div>
-          </div>
-
-        </Slider>
+          </Slider>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ProductsLike;
