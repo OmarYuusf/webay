7587ae493e8ba0358e4cbd34c783fa3f98518288
OmarYuusf <57051225+OmarYuusf@users.noreply.github.com>
2020-02-11 19:46:06 -0800

    fix select piece

--- src/Component/ProductPage/ProductsLike/ProductsLike.js
+++ src/Component/ProductPage/ProductsLike/ProductsLike.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { MDBIcon } from "mdbreact";
-import "./ProductsLike.css"
+import "./ProductsLike.css";
 // react-slick Import
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -8,6 +8,41 @@ import "slick-carousel/slick/slick-theme.css";
 
 const ProductsLike = () => {
   var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  var settingsSec = {
     dots: true,
     infinite: false,
     speed: 500,
@@ -46,232 +81,67 @@ const ProductsLike = () => {
     <div className="products-like">
       <h3>اختار قطعة</h3>
       <div className="products-like-container">
-        <div className="products-like-item">
-          <div className="item-head">
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>اللون</p>
-              </span>
-              <span>احمر</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>وحدة التخزين</p>
-              </span>
-              <span>32 جيجا</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>عرض الشاشة</p>
-              </span>
-              <span>12 بوصة</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>واي فاي</p>
-              </span>
-              <span>متواجد</span>
-            </div>
-          </div>
-          <div className="item-center">
-            <Slider {...settings}>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
+        <Slider {...settings}>
+          <div>
+            <div className="products-like-item">
+              <div className="item-head">
+                <div>
+                  <span>
+                    <MDBIcon icon="check-circle" />
+                    <p>اللون</p>
+                  </span>
+                  <span>احمر</span>
+                </div>
+                <div>
+                  <span>
+                    <MDBIcon icon="check-circle" />
+                    <p>وحدة التخزين</p>
+                  </span>
+                  <span>32 جيجا</span>
+                </div>
+                <div>
+                  <span>
+                    <MDBIcon icon="check-circle" />
+                    <p>عرض الشاشة</p>
+                  </span>
+                  <span>12 بوصة</span>
+                </div>
+                <div>
+                  <span>
+                    <MDBIcon icon="check-circle" />
+                    <p>واي فاي</p>
+                  </span>
+                  <span>متواجد</span>
+                </div>
               </div>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
+              <div className="item-center">
+                <Slider {...settingsSec}>
+                  <div>
+                    <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
+                  </div>
+                  <div>
+                    <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
+                  </div>
+                  <div>
+                    <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
+                  </div>
+                </Slider>
               </div>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
+              <div className="item-footer">
+                <h5>99 ريال</h5>
+                <div className="item-footer-discount">
+                  <p>135 ريال </p>
+                  <span>خصم 27 %</span>
+                </div>
+                <p className="discount-end">الخصم سينتهي خلال شهرين</p>
               </div>
-            </Slider>
-          </div>
-          <div className="item-footer">
-            <h5>99 ريال</h5>
-            <div className="item-footer-discount">
-              <p>135 ريال </p>
-              <span>خصم 27 %</span>
-            </div>
-            <p className="discount-end">الخصم سينتهي خلال شهرين</p>
-          </div>
-          <div className="item-button-select">
-            <button>اختار القطعة</button>
-          </div>
-        </div>
-        <div className="products-like-item">
-          <div className="item-head">
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>اللون</p>
-              </span>
-              <span>احمر</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>وحدة التخزين</p>
-              </span>
-              <span>32 جيجا</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>عرض الشاشة</p>
-              </span>
-              <span>12 بوصة</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>واي فاي</p>
-              </span>
-              <span>متواجد</span>
-            </div>
-          </div>
-          <div className="item-center">
-            <Slider {...settings}>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
+              <div className="item-button-select">
+                <button>اختار القطعة</button>
               </div>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
-              </div>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
-              </div>
-            </Slider>
-          </div>
-          <div className="item-footer">
-            <h5>99 ريال</h5>
-            <div className="item-footer-discount">
-              <p>135 ريال </p>
-              <span>خصم 27 %</span>
-            </div>
-            <p className="discount-end">الخصم سينتهي خلال شهرين</p>
-          </div>
-          <div className="item-button-select">
-            <button>اختار القطعة</button>
-          </div>
-        </div>
-        
-        <div className="products-like-item">
-          <div className="item-head">
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>اللون</p>
-              </span>
-              <span>احمر</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>وحدة التخزين</p>
-              </span>
-              <span>32 جيجا</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>عرض الشاشة</p>
-              </span>
-              <span>12 بوصة</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>واي فاي</p>
-              </span>
-              <span>متواجد</span>
             </div>
           </div>
-          <div className="item-center">
-            <Slider {...settings}>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
-              </div>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
-              </div>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
-              </div>
-            </Slider>
-          </div>
-          <div className="item-footer">
-            <h5>99 ريال</h5>
-            <div className="item-footer-discount">
-              <p>135 ريال </p>
-              <span>خصم 27 %</span>
-            </div>
-            <p className="discount-end">الخصم سينتهي خلال شهرين</p>
-          </div>
-          <div className="item-button-select">
-            <button>اختار القطعة</button>
-          </div>
-        </div>
-        <div className="products-like-item">
-          <div className="item-head">
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>اللون</p>
-              </span>
-              <span>احمر</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>وحدة التخزين</p>
-              </span>
-              <span>32 جيجا</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>عرض الشاشة</p>
-              </span>
-              <span>12 بوصة</span>
-            </div>
-            <div>
-              <span>
-                <MDBIcon icon="check-circle" />
-                <p>واي فاي</p>
-              </span>
-              <span>متواجد</span>
-            </div>
-          </div>
-          <div className="item-center">
-            <Slider {...settings}>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
-              </div>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
-              </div>
-              <div>
-                <img src="https://k.nooncdn.com/t_desktop-pdp-v1/v1570191588/N23423233A_1.jpg" />
-              </div>
-            </Slider>
-          </div>
-          <div className="item-footer">
-            <h5>99 ريال</h5>
-            <div className="item-footer-discount">
-              <p>135 ريال </p>
-              <span>خصم 27 %</span>
-            </div>
-            <p className="discount-end">الخصم سينتهي خلال شهرين</p>
-          </div>
-          <div className="item-button-select">
-            <button>اختار القطعة</button>
-          </div>
-        </div>
-        
+
+        </Slider>
       </div>
     </div>
   );
